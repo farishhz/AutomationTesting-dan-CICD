07ab03e541dce16e70429f8e5e49c6b0d0111cdc
Faris <alfarisiazmir14@gmail.com>
2026-04-19 13:05:31 +0700

    fix: disable cypress supportFile

--- cypress.config.js
+++ cypress.config.js
@@ -3,6 +3,7 @@ const { defineConfig } = require('cypress');
 module.exports = defineConfig({
   e2e: {
     baseUrl: 'http://localhost:4173',
+    supportFile: false,
     setupNodeEvents() {
       // implement node event listeners here
     },
